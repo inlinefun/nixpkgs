30c95f38d7bfa8d60ae146362d573a40913e7ebf
Samuel Ainsworth <skainsworth@gmail.com>
2025-06-17 23:33:20 -0400

    python3Packages.misaki: init at 0-unstable-2025-06-16

--- pkgs/development/python-modules/phonemizer/default.nix
+++ pkgs/development/python-modules/phonemizer/default.nix
@@ -4,6 +4,7 @@
   replaceVars,
   buildPythonPackage,
   fetchPypi,
+  fetchpatch,
   joblib,
   segments,
   attrs,
@@ -30,6 +31,12 @@ buildPythonPackage rec {
     (replaceVars ./backend-paths.patch {
       libespeak = "${lib.getLib espeak-ng}/lib/libespeak-ng${stdenv.hostPlatform.extensions.sharedLibrary}";
       # FIXME package festival
+    })
+    # This patch is needed for python3Packages.misaki. See https://github.com/thewh1teagle/espeakng-loader?tab=readme-ov-file#usage-with-phonemizer.
+    (fetchpatch {
+      name = "pr191-add-option-to-use-custom-espeak-data-path.patch";
+      url = "https://github.com/bootphon/phonemizer/commit/cc1db4bfaf688fdfb8275fd83d218f06411455e6.patch";
+      hash = "sha256-9TEEBmLrYU5Ftjclr1UBwA42sef7+4ToGACN/MrB86w=";
     })
   ];
 
